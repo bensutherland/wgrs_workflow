dc2d6f559488fa4fe6f4c8cf56ddd9711555e27f
bensutherland <bensutherland7@gmail.com>
2024-03-12 16:53:37 -0700

    Add option to remove duplicates using Picard Tools

--- 01_scripts/04_markduplicates.sh
+++ 01_scripts/04_markduplicates.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Mark duplicates in each BAM (PCR or optical)
+# Remove duplicates in each BAM (PCR or optical)
+#  note: set REM_DUPS=FALSE if you want to only mark duplicates
 
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
 # Set variables
 JAVA="/usr/bin/java" # full path needed
 PICARD="/usr/local/bin/picard-tools-1.131/picard.jar"        # full path needed
+REM_DUPS=true
 
 # Mark duplicates on all sorted bam files 
 for file in $(ls -1 04_samples/*.sorted.bam)
@@ -16,10 +18,8 @@ do
     # Calculate coverage
     $JAVA -jar $PICARD MarkDuplicates I=$file \
         O="${file%.bam}"_mdups.bam \
-        M="${file%.bam}"_mdups_metrics.txt 
-        #--OPTICAL_DUPLICATE_PIXEL_DISTANCE=2500 
+        M="${file%.bam}"_mdups_metrics.txt \
+        REMOVE_DUPLICATES=$REM_DUPS
 
 done 2>&1 | tee 10_log_files/"$TIMESTAMP"_picard_mdups.log
-
-
 
