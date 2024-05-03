e9de69a2936a36c5aedee1dde181fd02833e8562
GreenT <bensutherland7@gmail.com>
2024-05-03 11:34:29 -0700

    Update result assessment script

--- 01_scripts/assess_results.sh
+++ 01_scripts/assess_results.sh
@@ -4,45 +4,61 @@
 # Set variables
 FQ_SUFFIX="_R1.fastq.gz"
 BAM_SUFFIX=".sorted.bam"
+POSTDUP_SUFFIX=".sorted_mdups.bam"
 
 # Move into the directory containing files
 cd 04_samples 
 
-# Clear previous runs
-rm reads_per_sample.txt 2> /dev/null
-rm reads_per_sample_table.txt 2> /dev/null
-rm mappings_per_sample.txt 2> /dev/null 
-rm mappings_per_sample_table.txt 2> /dev/null
+# # Clear previous runs
+# rm reads_per_sample.txt 2> /dev/null
+# rm reads_per_sample_table.txt 2> /dev/null
+# rm mappings_per_sample.txt 2> /dev/null 
+# rm mappings_per_sample_table.txt 2> /dev/null
+# rm mappings_post_rmdups_sample.txt 2> /dev/null
+# rm mappings_post_rmdups_sample_table.txt 2> /dev/null
+# 
+# 
+# ## Number Reads
+# # Determine number of reads per file from samples file
+# for i in $(ls *$FQ_SUFFIX) ; 
+#     do echo $i ;
+#     gunzip -c $i | wc -l | awk 'END { print $1/4}' ;
+#     done >> reads_per_sample.txt
+# 
+# # Separate by second line into two columns
+# sed 'N;s/\n/ /' reads_per_sample.txt > reads_per_sample_table.txt
+# 
+# # Remove intermediate file
+# rm reads_per_sample.txt
+# 
+# 
+# ## Number Mappings
+# # Determine number of reads per file from samples file
+# for i in $(ls *$BAM_SUFFIX) ; 
+#     do echo $i ;
+#     samtools view $i | wc -l ;
+#     done >> mappings_per_sample.txt
+# 
+# # Separate by second line into two columns
+# sed 'N;s/\n/ /' mappings_per_sample.txt > mappings_per_sample_table.txt
+# 
+# # Remove intermediate file
+# rm mappings_per_sample.txt
+# 
 
-## Number Reads
+## Number Mappings, after dups removed
 # Determine number of reads per file from samples file
-for i in $(ls *$FQ_SUFFIX) ; 
-    do echo $i ;
-    gunzip -c $i | wc -l | awk 'END { print $1/4}' ;
-    done >> reads_per_sample.txt
-
-# Separate by second line into two columns
-sed 'N;s/\n/ /' reads_per_sample.txt > reads_per_sample_table.txt
-
-# Remove intermediate file
-rm reads_per_sample.txt
-
-
-## Number Mappings
-# Determine number of reads per file from samples file
-for i in $(ls *$BAM_SUFFIX) ; 
+for i in $(ls *$POSTDUP_SUFFIX) ; 
     do echo $i ;
     samtools view $i | wc -l ;
-    done >> mappings_per_sample.txt
+    done >> mappings_post_rmdups_sample.txt
 
 # Separate by second line into two columns
-sed 'N;s/\n/ /' mappings_per_sample.txt > mappings_per_sample_table.txt
+sed 'N;s/\n/ /' mappings_post_rmdups_sample.txt > mappings_post_rmdups_sample_table.txt
 
 # Remove intermediate file
-rm mappings_per_sample.txt
+rm mappings_post_rmdups_sample.txt
 
-# Move back to main directory to run rscript
-cd ..
 
 
 ## Reporting
